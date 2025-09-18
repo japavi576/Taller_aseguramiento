64f15b61212c6a90660a85d210bb23464f6341ab
japavi576 <japavi576@gmail.com>
2025-09-17 22:05:59 -0500

    Ultimo Ejercicio

--- Ejercicio5/contarVocales.js
+++ Ejercicio5/contarVocales.js
@@ -6,13 +6,13 @@ function contarVocales(texto) {
   const vocales = "aeiouáéíóú";
 
   let contador = 0;
-
+  // se recorre en busqueda de las vocales
   for (let letra of texto) {
     if (vocales.includes(letra)) {
       contador++;
     }
   }
-
+  // retornamos la variable
   return contador;
 }
 
